17d1aa9cc554d5e7beafc9fd2bb74e19b35de3ae
kzk-maeda <kzk.maeda0711@gmail.com>
2021-01-03 03:37:28 +0900

    post new

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,8 +60,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `KZK_MAEDA's Blog`,
+        short_name: `kzk blog`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
