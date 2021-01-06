54973c86725352356d8a445b92ea93ac40f6c164
kzk-maeda <kzk.maeda0711@gmail.com>
2021-01-07 02:28:00 +0900

    design changed

--- src/components/sns-share.tsx
+++ src/components/sns-share.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import {
     FacebookShareButton,
     FacebookIcon,
@@ -10,25 +11,41 @@ import {
     TwitterIcon
 } from 'react-share'
 
+export const Buttons = styled.div`
+    display: flex;
+    padding: 20px;
+    justify-content: center;
+`
+
+export const Button = styled.div`
+    margin-left: 20px;
+    margin-right: 20px
+`
+
 const SNSShare: React.FC<{title: string} & {articleUrl: string}> = ({title, articleUrl}) => {
     return (
-        <div>
-            <FacebookShareButton url={articleUrl}>
-                <FacebookIcon size={50} round />
-            </FacebookShareButton>
-
-            <LineShareButton title={title} url={articleUrl} >
-                <LineIcon size={50} round />
-            </LineShareButton>
-
-            <LinkedinShareButton title={title} url={articleUrl} >
-                <LinkedinIcon size={50} round />
-            </LinkedinShareButton>
-
-            <TwitterShareButton title={title} url={articleUrl} >
-                <TwitterIcon size={50} round />
-            </TwitterShareButton>
-        </div>
+        <Buttons>
+            <Button>
+                <FacebookShareButton url={articleUrl}>
+                    <FacebookIcon size={40} round />
+                </FacebookShareButton>
+            </Button>
+            <Button>
+                <LineShareButton title={title} url={articleUrl} >
+                    <LineIcon size={40} round />
+                </LineShareButton>
+            </Button>
+            <Button>
+                <LinkedinShareButton title={title} url={articleUrl} >
+                    <LinkedinIcon size={40} round />
+                </LinkedinShareButton>
+            </Button>
+            <Button>
+                <TwitterShareButton title={title} url={articleUrl} >
+                    <TwitterIcon size={40} round />
+                </TwitterShareButton>
+            </Button>
+        </Buttons>
     )
 }
 
