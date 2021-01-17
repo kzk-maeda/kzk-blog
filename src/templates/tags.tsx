8601db4a649f6fa5cd8ae1b62ec7b1a3b303313d
kzk-maeda <kzk.maeda0711@gmail.com>
2021-01-17 14:35:13 +0900

    re-design each tag list page

--- src/templates/tags.tsx
+++ src/templates/tags.tsx
@@ -4,6 +4,13 @@ import { Link, graphql, PageProps } from 'gatsby'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import NoteSharpIcon from '@material-ui/icons/NoteSharp';
+
 type Props = {
     tag: string
 }
@@ -24,16 +31,21 @@ const Tags: React.FC<PageProps<GatsbyTypes.TagsQuery, Props>> = ({ data, pageCon
         <Layout location={location} title={siteTitle}>
           <SEO title="All tags" />
           <h1>{tagHeader}</h1>
-          <ul>
+          <List>
               {edges.map( ({node})  => {
                 return (
-                    <li key={node.fields?.slug!}>
-                        <Link to={node.fields?.slug!}>{node.frontmatter?.title!}</Link>
-                    </li> 
+                    <ListItem key={node.fields?.slug!}>
+                        <ListItemAvatar>
+                            <NoteSharpIcon />
+                        </ListItemAvatar>
+                        <Link to={node.fields?.slug!}>
+                            <ListItemText primary={node.frontmatter?.title!} />
+                        </Link>
+                    </ListItem> 
                 )
               }
               )}
-          </ul>
+          </List>
             {/*
               This links to a page that does not yet exist.
               You'll come back to it!
