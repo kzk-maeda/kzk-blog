c1135c5752e5be48281b3861a1d5c0c23fb2c030
kzk-maeda <kzk.maeda0711@gmail.com>
2021-02-09 02:15:31 +0900

    cli initialized

--- src/components/cli.tsx
+++ src/components/cli.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { NoSsr } from '@material-ui/core';
 const { XTerm = {} } = typeof window !== `undefined` ? require("xterm-for-react") : {}
@@ -18,22 +18,36 @@ const useStyles = makeStyles((theme: Theme) =>
 const CLI = () => {
   const classes = useStyles();
   const [input, setInput] = useState("")
+  const [count, setCount] = useState(0)
   const xtermRef: React.RefObject<typeof XTerm> = React.createRef()
 
   useEffect(() => {
-    xtermRef.current?.terminal.writeln(
-      "Please enter any string then press enter:"
-    );
-    xtermRef.current?.terminal.write("echo > ");
+    console.log(xtermRef)
   }, []);
+  
+  useLayoutEffect(() => {
+    console.log("layout effect");
+    console.log(count)
+    // 初回のbackgroud renderと、toggle on時のrender
+    if (count < 2) {
+      xtermRef.current?.terminal.writeln(
+        "Please enter any string then press enter:"
+      );
+      xtermRef.current?.terminal.write("echo > ");
+      setCount(prevCount => prevCount + 1)
+    }
+  });
 
   return (
-    <NoSsr>
+    // XTermがdocument objectを操作するので、SSR時にbuild errorとなる
+    // それを回避するために、Xterm ComponentはSSRをスキップする
+    <NoSsr fallback="<>" defer={true}>
       <XTerm className={classes.cli}
         options={{ theme: {background: "brightBlack"} }}
         ref={xtermRef}
         onData={(data: string) => {
           const code = data.charCodeAt(0);
+          console.log(xtermRef)
           // console.log("data : " + data)
           // console.log("data.length : " + data.length)
           // console.log("charCodeAt : " + data.charCodeAt(0))
